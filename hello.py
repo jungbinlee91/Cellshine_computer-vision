e0f6f63c3d5189548a642f8ab142b83b2371043c
Jungbin <jblee@cellshine.co.kr>
2026-07-21 16:11:59 +0900

    Initialize Python development environment

---
+++ hello.py
@@ -0,0 +1,7 @@
+import platform
+import sys
+
+print("Python development environment is ready.")
+print(f"Python: {sys.version}")
+print(f"Executable: {sys.executable}")
+print(f"Platform: {platform.platform()}")
